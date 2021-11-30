0afd29bb59181175f833cc80353dbf8b6aa32de3
Tsang Han Wong <tsanghan@gmail.com>
2021-11-30 12:28:09 +0800

    added rancher local-path-provisioner

--- kubernetes-on-lxd/k-apply.sh
+++ kubernetes-on-lxd/k-apply.sh
@@ -3,3 +3,4 @@ kubectl apply -f https://raw.githubusercontent.com/tsanghan/content-cka-resource
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
