f47c3792825fb1f28959a56b77d3d77373fb8df9
Tsang Han Wong <tsanghan@gmail.com>
2021-12-05 11:19:31 +0800

    added cilium download script

---
+++ cilium/get-cilium.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Ref: https://docs.cilium.io/en/stable/gettingstarted/k8s-install-default/
+curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-amd64.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+rm cilium-linux-amd64.tar.gz{,.sha256sum}
+
