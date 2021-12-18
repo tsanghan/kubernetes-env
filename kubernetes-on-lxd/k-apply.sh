c5558ab082905280972cf0b2208dd441bd1f2e84
Wong Tsang Han <tsanghan@gmail.com>
2021-12-18 12:03:35 +0800

    fix more merge leftover

--- kubernetes-on-lxd/k-apply.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/tsanghan/content-cka-resources/master/metrics-server-components.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
