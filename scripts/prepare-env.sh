197ad56422292c105528704059895eb91f9bab71
Wong Tsang Han <tsanghan@gmail.com>
2021-12-24 10:17:09 +0800

    vimrc as resources file

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -356,21 +356,6 @@ then
 fi
 EOF
 
-cat <<'EOF' > ~/.vimrc
-colorscheme delek
-set nu rnu
-autocmd FileType yaml,yml,sh setlocal ts=2 sts=2 sw=2 et ai
-set pastetoggle=<F10>
-inoremap <Up> <Nop>
-inoremap <Down> <Nop>
-inoremap <Left> <Nop>
-inoremap <Right> <Nop>
-nnoremap <Up> <Nop>
-nnoremap <Down> <Nop>
-nnoremap <Left> <Nop>
-nnoremap <Right> <Nop>
-EOF
-
 cat <<'MYEOF' > ~/.local/bin/update_kubectl.sh
 #!/usr/bin/env bash
 
