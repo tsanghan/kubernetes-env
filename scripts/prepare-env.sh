10ce4118fd15f11ebea25e7391474de5fcec560a
Wong Tsang Han <tsanghan@gmail.com>
2021-12-24 16:35:16 +0800

    add more common checking logic

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -547,9 +547,13 @@ for c in ctrlp-1 wrker-1 wrker-2; do
   lxc launch -p k8s "$image" lxd-"$c"
 done
 
-check_lxd_status STOP 3 .
-lxc start --all
-check_lxd_status eth0 3 \!
+common=$(lxc image ls | grep lxd-common)
+if [ "common" == "" ]; then
+  check_lxd_status STOP 3 .
+  lxc start --all
+  check_lxd_status eth0 3 \!
+fi
+
 IPADDR=$(lxc ls | grep ctrlp | awk '{print $6}')
 update_local_etc_hosts "$IPADDR"
 echo
@@ -633,11 +637,15 @@ for c in ctrlp-1 ctrlp-2 ctrlp-3 wrker-1 wrker-2 wrker-3; do
   lxc launch -p k8s "$image" lxd-"$c"
 done
 
-check_lxd_status STOP 7 .
-lxc start lxd-ctrlp-1 lxd-ctrlp-2 lxd-ctrlp-3
-sleep 8
-lxc start --all
-check_lxd_status eth0 7 \!
+common=$(lxc image ls | grep lxd-common)
+if [ "common" == "" ]; then
+  check_lxd_status STOP 7 .
+  lxc start lxd-ctrlp-1 lxd-ctrlp-2 lxd-ctrlp-3
+  sleep 8
+  lxc start --all
+  check_lxd_status eth0 7 \!
+fi
+
 IPADDR=$(lxc ls | grep lb | awk '{print $6}')
 update_local_etc_hosts "$IPADDR"
 sleep 8
