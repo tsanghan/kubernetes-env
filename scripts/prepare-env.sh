ca92f5e059d0c40206ee42f353ed049a3fb33c7d
Wong Tsang Han <tsanghan@gmail.com>
2021-12-24 12:55:00 +0800

    fixed -f file path

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -649,38 +649,40 @@ fi
 MYEOF
 
 # Install kubectl
-KUBECTL_VER=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-curl -sSL -o /tmp/kubectl "https://dl.k8s.io/$KUBECTL_VER/bin/linux/amd64/kubectl"
-KUBECTL_SHA256=$(curl -sSL https://dl.k8s.io/"$KUBECTL_VER"/bin/linux/amd64/kubectl.sha256)
-OK=$(echo "$KUBECTL_SHA256" /tmp/kubectl | sha256sum --check)
-if [[ ! "$OK" =~ .*OK$ ]]; then
-  echo "kubectl binary does not match sha256 checksum, aborting!!"
-  rm /tmp/kubectl
-  exit $?
-else
-  echo "Installing kubectl verion=$KUBECTL_VER"
-  mv /tmp/kubectl ~/.local/bin/kubectl
-  chmod +x ~/.local/bin/kubectl
+if [ ! -f ~/.local/bin/kubectl ]; then
+  KUBECTL_VER=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+  curl -sSL -o /tmp/kubectl "https://dl.k8s.io/$KUBECTL_VER/bin/linux/amd64/kubectl"
+  KUBECTL_SHA256=$(curl -sSL https://dl.k8s.io/"$KUBECTL_VER"/bin/linux/amd64/kubectl.sha256)
+  OK=$(echo "$KUBECTL_SHA256" /tmp/kubectl | sha256sum --check)
+  if [[ ! "$OK" =~ .*OK$ ]]; then
+    echo "kubectl binary does not match sha256 checksum, aborting!!"
+    rm /tmp/kubectl
+    exit $?
+  else
+    echo "Installing kubectl verion=$KUBECTL_VER"
+    mv /tmp/kubectl ~/.local/bin/kubectl
+    chmod +x ~/.local/bin/kubectl
+  fi
 fi
 
 # Install kind
-if [ ! -x ~/local/bin/kind ]; then
+if [ ! -f ~/.local/bin/kind ]; then
   curl -sSL -o ~/.local/bin/kind \
     "$(curl -s https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq ".assets[].browser_download_url" | grep amd64 | grep linux | tr -d '"')"
 fi 
 # Install k9s
-if [ ! -x ~/local/bin/k9s ]; then
+if [ ! -f ~/.local/bin/k9s ]; then
   K9S_FRIEND=$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest | jq ".assets[].browser_download_url" | grep x86_64 | grep Linux | tr -d '"')
   curl -sSL "$K9S_FRIEND" | tar -C ~/.local/bin -zxvf - "$(basename \""$K9S_FRIEND\"" | sed 's/\(.*\)_Linux_.*/\1/')"
 fi
 # Install yq
-if [ ! -x ~/local/bin/yq ]; then
+if [ ! -f ~/.local/bin/yq ]; then
 curl -sSL -o ~/.local/bin/yq \
   "$(curl -s https://api.github.com/repos/mikefarah/yq/releases/latest | jq ".assets[].browser_download_url" | grep -v "tar.gz" | grep amd64 | grep linux | tr -d '"')"
 fi
 
 # Install shellcheck
-if [ ! -x ~/local/bin/shellcheck ]; then
+if [ ! -f ~/.local/bin/shellcheck ]; then
   SHELLCHECK=$(curl -s https://api.github.com/repos/koalaman/shellcheck/releases/latest | jq ".assets[].browser_download_url" | grep x86_64 | grep linux | tr -d '"')
   SHELLCHECK_DIR=$(basename "$SHELLCHECK" | sed 's/\(^.*v.*\).linux.*/\1/')
   SHELLCHECK_BIN=$(basename "$SHELLCHECK" | sed 's/\(.*\)-v.*/\1/')
@@ -690,7 +692,7 @@ if [ ! -x ~/local/bin/shellcheck ]; then
 fi
 
 # Install kubectx & kubens
-if ! [ -x ~/.local/bin/kubectx || -x ~/.local/bin/kubens ]; then
+if [ ! -f ~/.local/bin/kubectx ] || [ ! -f ~/.local/bin/kubens ]; then
   KUBE_FRIENDS=$(curl -s https://api.github.com/repos/ahmetb/kubectx/releases/latest | jq ".assets[].browser_download_url" | grep x86_64 | grep linux | tr -d '"')
   for friend in $KUBE_FRIENDS
   do
