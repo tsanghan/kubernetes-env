1c388e6964d603535517485e3ada7e5b05777d57
Wong Tsang Han <tsanghan@gmail.com>
2022-01-05 14:35:51 +0800

    fixed containerd and crun image pull

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -1150,14 +1150,14 @@ mkdir -p /tmp/"$USER"
 CONTAINERD_LATEST=$(curl -s https://api.github.com/repos/containerd/containerd/releases/latest)
 CONTAINERD_VER=$(echo -E "$CONTAINERD_LATEST" | jq -M ".tag_name" | tr -d '"' | sed 's/.*v\(.*\)/\1/')
 echo "Downloading Containerd v$CONTAINERD_VER..."
-CONTAINERD_URL=$(echo -E "$CONTAINERD_LATEST" | jq -M ".assets[].browser_download_url" | grep amd64 | grep linux | grep cri | tr -d '"')
+CONTAINERD_URL=$(echo -E "$CONTAINERD_LATEST" | jq -M ".assets[].browser_download_url" | grep amd64 | grep linux | grep cri | grep -v sha256 | tr -d '"')
 curl -L --remote-name-all "$CONTAONERD_URL"{,.sha256sum}
 sha256sum --check $(basename $CONTAINERD_URL).sha256sum
 
 CRUN_LATEST=$(curl -s https://api.github.com/repos/containers/crun/releases/latest)
 CRUN_VER=$(echo -E "$CRUN_LATEST" | jq -M ".tag_name" | tr -d '"' | sed 's/.*v\(.*\)/\1/')
 echo "Downloading Crun v$CRUN_VER..."
-CRUN_URL=$(echo -E "$CRUN_LATEST" | jq -M ".assets[].browser_download_url" | grep amd64 | grep linux | tr -d '"')
+CRUN_URL=$(echo -E "$CRUN_LATEST" | jq -M ".assets[].browser_download_url" | grep amd64 | grep linux | grep -v asc | tr -d '"')
 curl -L --remote-name-all "$CONTAONERD_URL"{,.asc}
 
 popd
