d77f751c20e3a6d7ead515d1291ec29bf79f6848
Wong Tsang Han <tsanghan@gmail.com>
2022-01-14 10:39:46 +0800

    fix vagrant created VM with 2 NICs & kubelet

---
+++ kubernetes-on-virtualbox/modify-kubelet.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+ip link dev enp0s8 up
+dhclient enp0s8
+while true; do
+  IPADDR=$(ip a s | grep "enp0s8$" | awk '{print $2}' | awk -F/ '{print $1}')
+  if [ "$IPADDR" != "" ]; then
+    break;
+  fi
+  sleep 2
+done
+sed -i "s/\(^ExecStart=.*KUBELET_EXTRA_ARGS$\)/\1 --node-ip=$IPADDR/" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
