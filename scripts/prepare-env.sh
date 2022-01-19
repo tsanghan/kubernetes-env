8084674aedf95adb258885c358e87e11cf5d984c
Wong Tsang Han <tsanghan@gmail.com>
2022-01-19 18:36:59 +0800

    reduce 1 more line after usage

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -1160,7 +1160,7 @@ usage() {
   echo '       -m   "Multi-control-plane mode"'
   echo '       -n   "Install CNI. Only 2 options"'
   echo '       -i   "Install Ingress. Only 2 options. F5/NGINX Ingress Controller/AP installation not enabled yet."'
-  echo -e '\n\n'
+  echo -e '\n'
   exit 1
 }
 
