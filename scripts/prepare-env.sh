d6efe82e773fb9ff9eafa98429daf1371e408fcd
Wong Tsang Han <tsanghan@gmail.com>
2022-02-04 18:04:32 +0800

    fix typo

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -1596,7 +1596,7 @@ cat <<'MYEOF' > ~/.local/bin/stop-nfs-server.sh
 
 nfs_deploy=$(kubectl get deployment nfs-subdir-external-provisioner 2>&1)
 if [[ ! "$nfs_deploy" =~ ^Error.* ]]; then
-  heml uninstall nfs-subdir-external-provisioner
+  helm uninstall nfs-subdir-external-provisioner
 fi
 nfs_server=$(lxc ls | grep nfs)
 if [ "$nfs_server" == "" ]; then
