eb90fc0e25956061768967d7eb784f4762425e45
Wong Tsang Han <tsanghan@gmail.com>
2022-02-08 13:51:53 +0800

    re-enable crun download

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -1562,18 +1562,18 @@ CONTAINERD_URL=$(echo -E "$CONTAINERD_LATEST" | jq -M ".assets[].browser_downloa
 curl -L --remote-name-all "$CONTAINERD_URL"{,.sha256sum}
 sha256sum --check "$(basename "$CONTAINERD_URL")".sha256sum
 
-# CRUN_LATEST=$(curl -s https://api.github.com/repos/containers/crun/releases/latest)
-# CRUN_VER=$(echo -E "$CRUN_LATEST" | jq -M ".tag_name" | tr -d '"' | sed 's/.*v\(.*\)/\1/')
-# echo "Downloading Crun v$CRUN_VER..."
-# echo
-# echo "***************************"
-# echo "*                         *"
-# echo "* Downloading Crun v$CRUN_VER *"
-# echo "*                         *"
-# echo "***************************"
-# echo
-# CRUN_URL=$(echo -E "$CRUN_LATEST" | jq -M ".assets[].browser_download_url" | grep amd64 | grep linux | grep -v asc | grep -v systemd | tr -d '"')
-# curl -L --remote-name-all "$CRUN_URL"{,.asc}
+CRUN_LATEST=$(curl -s https://api.github.com/repos/containers/crun/releases/latest)
+CRUN_VER=$(echo -E "$CRUN_LATEST" | jq -M ".tag_name" | tr -d '"' | sed 's/.*v\(.*\)/\1/')
+echo "Downloading Crun v$CRUN_VER..."
+echo
+echo "***************************"
+echo "*                         *"
+echo "* Downloading Crun v$CRUN_VER *"
+echo "*                         *"
+echo "***************************"
+echo
+CRUN_URL=$(echo -E "$CRUN_LATEST" | jq -M ".assets[].browser_download_url" | grep amd64 | grep linux | grep -v asc | grep -v systemd | tr -d '"')
+curl -L --remote-name-all "$CRUN_URL"{,.asc}
 
 popd || exit
 MYEOF
