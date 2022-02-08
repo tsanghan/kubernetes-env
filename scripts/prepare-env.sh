2729cd39064677250f108369eefdfe8d8f688869
Wong Tsang Han <tsanghan@gmail.com>
2022-02-08 09:25:47 +0800

    fix IPADDR var name

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -1373,7 +1373,7 @@ if [ "$multimaster" == "true" ]; then
   update_local_etc_hosts "$IPADDR"
 fi
 
-lxc exec lxd-ctrlp-1 -- kubeadm init --control-plane-endpoint "$CTRLP":6443 --upload-certs --apiserver-cert-extra-sans apiserver."$IPADDRESS".nip.io | tee kubeadm-init.out
+lxc exec lxd-ctrlp-1 -- kubeadm init --control-plane-endpoint "$CTRLP":6443 --upload-certs --apiserver-cert-extra-sans apiserver."$IPADDR".nip.io | tee kubeadm-init.out
 echo
 if [ ! -d ~/.kube ]; then
   mkdir ~/.kube
