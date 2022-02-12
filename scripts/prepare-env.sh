13b0e5f35b700986682deb696c816677d25239e9
Wong Tsang Han <tsanghan@gmail.com>
2022-02-12 14:56:22 +0800

    change nip.io name to contain "-"

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -1414,7 +1414,7 @@ if [ "$multimaster" == "true" ]; then
   update_local_etc_hosts "$IPADDR"
 fi
 
-lxc exec lxd-ctrlp-1 -- kubeadm init --control-plane-endpoint "$CTRLP":6443 --upload-certs --apiserver-cert-extra-sans apiserver."$IPADDR".nip.io | tee kubeadm-init.out
+lxc exec lxd-ctrlp-1 -- kubeadm init --control-plane-endpoint "$CTRLP":6443 --upload-certs --apiserver-cert-extra-sans apiserver-$(echo "$IPADDR" | sed 's/\./-/g').nip.io | tee kubeadm-init.out
 echo
 if [ ! -d ~/.kube ]; then
   mkdir ~/.kube
