580ea3e81c007d3189d0f07637beaa04f7948c12
Wong Tsang Han <tsanghan@gmail.com>
2022-04-15 19:50:39 +0800

    change kic namespace

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -546,7 +546,7 @@ echo
 #   --namespace ingress-nginx --create-namespace
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm install ingress-nginx ingress-nginx/ingress-nginx \
-  --namespace kubernetes-ingress-nginx \
+  --namespace ingress-nginx \
   --create-namespace \
   --set controller.ingressClassResource.name=kubernetes-ingress-nginx
 EOF
