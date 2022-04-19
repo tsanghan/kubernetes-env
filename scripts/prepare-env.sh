ce585a96b6c9078673874280da7f1681d0ad182b
Wong Tsang Han <tsanghan@gmail.com>
2022-04-19 22:08:55 +0800

    removed ingress-kic.sh comments

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -540,10 +540,7 @@ echo "* Deploy Kubernetes Ingress-NGINX Controller *"
 echo "*                                            *"
 echo "**********************************************"
 echo
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.2/deploy/static/provider/cloud/deploy.yaml
-# helm upgrade --install ingress-nginx ingress-nginx \
-#   --repo https://kubernetes.github.io/ingress-nginx \
-#   --namespace ingress-nginx --create-namespace
+
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx \
