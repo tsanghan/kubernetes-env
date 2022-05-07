88f10603e5d4c2f6538a95b2e88a6fcba21fcf5a
Wong Tsang Han <tsanghan@gmail.com>
2022-05-07 16:40:04 +0800

    roll back wiht ip_vs

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -779,7 +779,7 @@ else
 
     cat <<-EOF | lxc profile edit k8s-cloud-init
     config:
-      linux.kernel_modules: ip_vs,ip_vs_rr,ip_vs_wrr,ip_vs_sh,ip_tables,ip6_tables,netlink_diag,nf_nat,overlay,br_netfilter
+      linux.kernel_modules: ip_tables,ip6_tables,netlink_diag,nf_nat,overlay,br_netfilter
       raw.lxc: |-
         lxc.apparmor.profile=unconfined
         lxc.mount.auto=proc:rw sys:rw cgroup:rw
@@ -902,32 +902,20 @@ else
         type: unix-block
       aadisable:
         path: /sys/module/nf_conntrack/parameters/hashsize
-        source: /sys/module/nf_conntrack/parameters/hashsize
+        source: /dev/null
         type: disk
-      # aadisable2:
-      #   path: /dev/zfs
-      #   source: /dev/zfs
-      #   type: disk
       aadisable1:
         path: /sys/module/apparmor/parameters/enabled
-        source: /sys/module/apparmor/parameters/enabled
-        type: disk
-      aadisable3:
-        path: /dev/kmsg
-        source: /dev/kmsg
-        type: unix-char
-      aadisable4:
-        path: /sys/fs/bpf
-        source: /sys/fs/bpf
-        type: disk
-      aadisable5:
-        path: /proc/sys/net/netfilter/nf_conntrack_max
-        source: /proc/sys/net/netfilter/nf_conntrack_max
+        source: /dev/null
         type: disk
       boot_dir:
         path: /boot
         source: /boot
         type: disk
+      dev_kmsg:
+        path: /dev/kmsg
+        source: /dev/kmsg
+        type: unix-char
       eth0:
         name: eth0
         nictype: bridged
